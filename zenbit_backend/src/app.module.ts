e4f5c5b1acf4a55ab074ed7a834c0a48527eaa48
valera <valerazakolotiuk@gmail.com>
2026-04-01 19:32:25 +0300

    Init

--- zenbit_backend/src/app.module.ts
+++ zenbit_backend/src/app.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { UsersModule } from './users/users.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-    UsersModule,
-  ],
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
